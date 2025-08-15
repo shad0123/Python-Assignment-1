6b0915bd225da70e22a4fa4daee0c2c1d2c50d1a
shad0123 <adityahati01@gmail.com>
2025-08-15 15:26:56 +0530

    Add task1.py and task2.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+fn = int(input('Enter the first number: '))
+sn = int(input('Enter the second number: '))
+
+add = fn + sn
+sub = fn - sn
+mul = fn * sn
+div = fn / sn
+
+print('Addition: ',add)
+print('Subtraction: ',sub)
+print('Multiplication: ',mul)
+print('Division: ',div)
---
+++ task2.py
@@ -0,0 +1,5 @@
+fn = input("Enter your first name: ")
+ln = input("Enter your last name: ")
+
+fullname = fn + " " + ln + "!"
+print("Hello,",fullname,"Welcome to the Python program.")
